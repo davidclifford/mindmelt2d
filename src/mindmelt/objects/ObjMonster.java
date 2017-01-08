dc1cddee6a444ee1864c547c7f7fae7a41101834
w18749 <w18749@L140808.corp.org.local>
2017-01-08 21:31:01 +1100

    Get monsters to follow you if close

--- src/mindmelt/objects/ObjMonster.java
+++ src/mindmelt/objects/ObjMonster.java
@@ -5,15 +5,37 @@ import mindmelt.maps.World;
 
 public class ObjMonster extends Obj {
 
+    public static final int SMELL_DIST = 5;
+    
     @Override
     public void update(Engine engine, int delta) {
+        Obj player = engine.getObjects().getPlayer();
+        int px = player.getX();
+        int py = player.getY();
         ObjMonster mon = this;
         int dx = x;
         int dy = y;
-        if (rand.nextInt(2)==0) {
-            dx += rand.nextInt(2)*2-1;
+        
+        int difx = px - x;
+        int dify = py - y;
+        if(difx*difx+dify*dify > SMELL_DIST*SMELL_DIST) {
+            if (rand.nextInt(2)==0) {
+                dx += rand.nextInt(2)*2-1;
+            } else {
+                dy += rand.nextInt(2)*2-1;
+            }
         } else {
-            dy += rand.nextInt(2)*2-1;
+            int nx = sign(px-x);
+            int ny = sign(py-y);
+            if (Math.abs(nx) + Math.abs(ny) == 2) {
+                if (rand.nextInt(2)==0) {
+                    nx = 0;
+                } else {
+                    ny = 0;
+                }
+            }
+            dx += nx;
+            dy += ny;
         }
         if(isReady(delta)) {
             if(engine.canEnter(this,dx,dy,z)) {
@@ -22,5 +44,9 @@ public class ObjMonster extends Obj {
                 engine.activateTile(dx, dy, z);
             }
         }
+    }
+    
+    private int sign(int s) {
+        return (s<0) ? -1 : (s>0) ? 1 : 0;
     }
 }
